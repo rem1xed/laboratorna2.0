c079e2cc9bd4bc66c304388a4def4239e25715b4
rem1xed <mykhailolypko2006@gmail.com>
2024-09-15 20:21:51 +0300

    edit1

---
+++ program_laboratorna_2.1/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double z1, z2, m;
+
+	cout << "m = \t"; cin >> m;
+
+	z1 = ((sqrt(pow((3 * m + 2), 2) - 24 * m))) / (3 * sqrt(m) - 2 / sqrt(m));
+	z2 = sqrt(m);
+
+	cout << endl << "z1: " << z1;
+	cout << endl << "z2: " << z2;
+
+	cin.get();
+	//edit 1
+	return 0;
+}
